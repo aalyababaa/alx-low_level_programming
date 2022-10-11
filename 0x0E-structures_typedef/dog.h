b96de5a1f952269d520a9204df96da7ecc21430a
aalyababaa <aalyafarooqui770@gmail.com>
2022-10-11 02:52:08 -0700

    0.poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_H_FILE
+#define DOG_H_FILE
 
 /**
- * struct dog - structure
- * @name: name
- * @age: age
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
  * @owner: owner
  */
 typedef struct dog
@@ -13,10 +13,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
